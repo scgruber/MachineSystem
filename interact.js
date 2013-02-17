8418875ee823245425136102721a013f73e50ebb
Sam Gruber <grubermensch@gmail.com>
2013-02-17 15:18:45 -0500

    fix keypress

--- interact.js
+++ interact.js
@@ -1,24 +1,26 @@
 // Keyboard control of pan and zoom
 
-$(document).keydown(function() {
-  switch (event.which) {
-    case 65: // A (Left)
-      glData.view.x++;
-      break;
-    case 68: // D (Right)
-      glData.view.x--;
-      break;
-    case 83: // S (Down)
-      glData.view.y--;
-      break;
-    case 87: // W (Up)
-      glData.view.y++;
-      break;
-    case 88: // X (Out)
-      glData.view.z--;
-      break;
-    case 90: // Z (In)
-      glData.view.z++;
-      break;
-  }
+$(document).ready(function() {
+  $(document).keydown(function() {
+    switch (event.which) {
+      case 65: // A (Left)
+        glData.view.x++;
+        break;
+      case 68: // D (Right)
+        glData.view.x--;
+        break;
+      case 83: // S (Down)
+        glData.view.y--;
+        break;
+      case 87: // W (Up)
+        glData.view.y++;
+        break;
+      case 88: // X (Out)
+        glData.view.z--;
+        break;
+      case 90: // Z (In)
+        glData.view.z++;
+        break;
+    }
+  });
 });
