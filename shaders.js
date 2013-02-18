cfd54cb41571a8b447f2afd488314f1cf258b4ec
Sam Gruber <grubermensch@gmail.com>
2013-02-17 20:09:16 -0500

    syntax bug

--- shaders.js
+++ shaders.js
@@ -24,6 +24,8 @@ shaderScripts['vert'] = {
     'uniform mat4 uMVMatrix;',
     'uniform mat4 uPMatrix;',
     '',
+    'varying vec4 vColor;',
+    '',
     'void main(void) {',
     '  gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);',
     '  vColor = aVertexColor;',
