1f05867a1449f1e73a411385783c109869a69e51
Sam Gruber <grubermensch@gmail.com>
2013-02-17 19:44:26 -0500

    more scaling adjustments

--- getdata.js
+++ getdata.js
@@ -135,7 +135,7 @@ Phys.prototype.addVirtualServer = function(vServer) {
 Phys.prototype.draw = function() {
   // Update code
   this.orbitRadius = (this.orbitRadius + (this.mem/50000.0))/2;
-  this.speed = (this.speed + (0.01 * this.cpu / 1000.0)) / 1.01;
+  this.speed = (this.speed + (0.001 * this.cpu / 1000.0)) / 1.001;
   this.theta = (this.theta + this.speed) % (2*Math.PI);
   this.radius = (this.radius + (this.disk/1000000.0))/2;
 
@@ -143,7 +143,7 @@ Phys.prototype.draw = function() {
   mvPushMatrix();
 
   mat4.rotate(glData.mvMatrix, this.theta, [0, 0, 1]);
-  mat4.translate(glData.mvMatrix, [this.orbitRadius, 0, 0]);
+  mat4.translate(glData.mvMatrix, [this.orbitRadius+2, 0, 0]);
   mat4.scale(glData.mvMatrix, [this.radius, this.radius, this.radius]);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, glData.buf.nodeVertexPos);
@@ -180,7 +180,7 @@ Virt.prototype.update = function(serverData) {
 Virt.prototype.draw = function() {
     // Update code
   this.orbitRadius = (this.orbitRadius + (this.mem/500000.0))/2;
-  this.speed = (this.speed + (0.01 * this.cpu / 1000.0)) / 1.01;
+  this.speed = (this.speed + (0.001 * this.cpu / 1000.0)) / 1.001;
   this.theta = (this.theta + this.speed) % (2*Math.PI);
   this.radius = (this.radius + (this.disk/10000000.0))/2;
 
@@ -188,7 +188,7 @@ Virt.prototype.draw = function() {
   mvPushMatrix();
 
   mat4.rotate(glData.mvMatrix, this.theta, [0, 0, 1]);
-  mat4.translate(glData.mvMatrix, [this.orbitRadius, 0, 0]);
+  mat4.translate(glData.mvMatrix, [this.orbitRadius+1, 0, 0]);
   mat4.scale(glData.mvMatrix, [this.radius, this.radius, this.radius]);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, glData.buf.nodeVertexPos);
