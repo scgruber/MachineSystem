f97155a4fee251cbdb61da060a32be79be1cee7d
Sam Gruber <grubermensch@gmail.com>
2013-02-18 02:59:37 -0500

    rescale interactivity

--- interact.js
+++ interact.js
@@ -4,22 +4,22 @@ $(document).ready(function() {
   $(document).keydown(function(event) {
     switch (String.fromCharCode(event.which)) {
       case 'A': // A (Left)
-        glData.view.x++;
+        glData.view.x+=5;
         break;
       case 'D': // D (Right)
-        glData.view.x--;
+        glData.view.x-=5;
         break;
       case 'S': // S (Down)
-        glData.view.y--;
+        glData.view.y-=5;
         break;
       case 'W': // W (Up)
-        glData.view.y++;
+        glData.view.y+=5;
         break;
       case 'X': // X (Out)
-        glData.view.z--;
+        glData.view.z-=5;
         break;
       case 'Z': // Z (In)
-        glData.view.z++;
+        glData.view.z+=5;
         break;
     }
   });
